e4909aaab8f1c28bb94a63306564c5b4da19074b
drsimonj <drsimonjackson@gmail.com>
2017-03-22 08:27:45 +1100

    Add: Dummy codings

--- projects/capstone/scripts/data_imdb.py
+++ projects/capstone/scripts/data_imdb.py
@@ -13,4 +13,18 @@ def prep(data_directory):
 	# 'movie_title', 'movie_imdb_link'
 	# should also be dropped eventually, but are needed to merge with other data sets
 
+	# Dummy code `language` to `english` = 1 (yes) or 0 (no)
+	# Because english language dominates large proportion
+	data['english'] = data['language'] == 'English'
+	data.drop(['language'], axis=1, inplace=True)
+
+	# Dummy code color to 1 (color) or 0 (Black and white)
+	data['color'] = data['color'] == 'Color'
+
+	# Dummy code genres
+	dummy_genres = data.genres.str.lower().str.get_dummies(sep='|')
+	data = pd.concat([data, dummy_genres], axis=1, join='inner')
+	data.drop(['genres'], axis=1, inplace=True)
+
+
 	return data
