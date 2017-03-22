bc178b167fb1ddf7dc9e5bf10c7996b98ce286ba
drsimonj <drsimonjackson@gmail.com>
2017-03-23 09:21:26 +1100

    Add: similarity tests

--- projects/capstone/scripts/data_imdb.py
+++ projects/capstone/scripts/data_imdb.py
@@ -1,10 +1,20 @@
 import os.path
 import pandas as pd
+import numpy as np
+
+from scipy.stats import norm
 from scripts.utility import help_read_csv
+
 
 def prep(data_directory):
 
 	print 'Preparing IMDB data...\n'
+
+	"""
+
+	SETUP
+
+	"""
 
 	# Read in data and print shape
 	data = help_read_csv(os.path.join(data_directory, 'movie_metadata.csv'))
@@ -16,8 +26,23 @@ def prep(data_directory):
 
 	print '- Dropped unneeded variables'
 
-	# 'movie_title', 'movie_imdb_link'
+	# Extract IMDB movie id from link as `imdb_id`
+	data['movie_imdb_link'] = data['movie_imdb_link'].astype('str')
+	data['imdb_id'] = data.movie_imdb_link.str.extract('(tt[0-9]+)', expand = False)
+	data['imdb_id'] = data['imdb_id'].str.extract('([0-9]+)', expand = False)
+	data.drop(['movie_imdb_link'], axis=1, inplace=True)
+
+	print '- Extracted IMDB movie ids as `imdb_id` from url in `movie_imdb_link`'
+
+
+	# 'movie_title', 'imdb_id'
 	# should also be dropped eventually, but are needed to merge with other data sets
+
+	"""
+
+	CATEGORICAL VARIABLES
+
+	"""
 
 	# Dummy code `language` to `english` = 1 (yes) or 0 (no)
 	# Because english language dominates large proportion
@@ -53,26 +78,48 @@ def prep(data_directory):
 
 	print '- Dummy coded `genres` to multiple columns'
 
-	# Extract IMDB movie id from link as `imdb_id`
-	data['movie_imdb_link'] = data['movie_imdb_link'].astype('str')
-	data['imdb_id'] = data.movie_imdb_link.str.extract('(tt[0-9]+)', expand = False)
-	data['imdb_id'] = data['imdb_id'].str.extract('([0-9]+)', expand = False)
-	data.drop(['movie_imdb_link'], axis=1, inplace=True)
+	"""
 
-	print '- Extracted IMDB movie ids as `imdb_id` from url in `movie_imdb_link`'
+	CONTINUOUS VARIABLES
+
+	"""
+
+	float_vars = list(data.select_dtypes(include=['float64']))
+
+	# Distribution Transformations
+	data.num_critic_for_reviews = np.sqrt(data.num_critic_for_reviews)
+	data.gross = np.sqrt(data.gross)
+
+	# An extreme option. See http://stackoverflow.com/questions/38151261/find-a-python-transformation-function-or-numpy-matrix-to-transform-skewed-normal
+	#data.num_critic_for_reviews = norm.ppf((data.num_critic_for_reviews.rank() - .5) / len(data))
+
+
+	print '- Distribution adjustments applied to selected float variables'
+
 
 	# Normalize (0-1) all float columns
-	cols_to_norm = list(data.select_dtypes(include=['float64']))
-	data[cols_to_norm] = data[cols_to_norm].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+	data[float_vars] = data[float_vars].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
 
 	print '- All float variables normalized to range between 0 to 1'
 
+	"""
+
+	MISSING VALUES
+
+	"""
 
 	# Handling missing values...
-	data.fillna(data.mean()[cols_to_norm], inplace = True)  # For float columns
-	data.fillna(data.drop(cols_to_norm, axis=1).mode(), inplace=True)  # For all others
+	data.fillna(data.mean()[float_vars], inplace=True)  # For float columns
+	data.fillna(data.drop(float_vars, axis=1).mode(), inplace=True)  # For all others
 
-	print '- Missing values imputed'
+	print '- Missing values imputed as mean/mode'
+
+	"""
+
+	END
+
+	"""
+
 
 	print '\nPrep of IMDB data completed.\n'
 	return data
