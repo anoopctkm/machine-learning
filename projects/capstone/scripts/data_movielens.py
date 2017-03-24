2d4f0065fc5cb87eb19a6132e8b951cadbc73296
drsimonj <drsimonjackson@gmail.com>
2017-03-24 11:04:13 +1100

    Add: Bind imdb id to user ratings

--- projects/capstone/scripts/data_movielens.py
+++ projects/capstone/scripts/data_movielens.py
@@ -19,6 +19,14 @@ def prep(data_directory):
     data = help_read_csv(os.path.join(data_directory, 'rating.csv'))
     print '- Read in .csv file with {} rows and {} columns'.format(*data.shape)
 
+    # Bind IMDB ID using link data
+    link = help_read_csv(os.path.join(data_directory, 'link.csv'))
+    link = link.drop('tmdbId', axis = 1)
+    data = data.join(link.set_index('movieId'), on = 'movieId', how = 'left')
+    data = data.rename(columns={'imdbId': 'imdb_id'}).drop('movieId', axis=1)
+    del link
+
+    print '- Bind IMDB ID as `imdb_id` using link.csv'
 
     """
 
